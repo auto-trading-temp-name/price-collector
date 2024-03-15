9f89f61ea10b04b3f9c274fbffaaea47b2c95b00
CelestialCrafter <celestialcraftermc@gmail.com>
2024-03-15 01:22:21 -0500

    add ?since reverse the list in get_prices before returning to client

--- src/api.rs
+++ src/api.rs
@@ -22,6 +22,7 @@ fn get_prices(
 	client: &Client,
 	mut amount: u16,
 	interval: u16,
+	before: Option<i64>,
 ) -> Result<Vec<Datapoint>> {
 	let collection_interval_minutes = (COLLECTION_INTERVAL.std_duration().as_secs() / 60) as u16;
 	if interval < collection_interval_minutes {
@@ -32,19 +33,38 @@ fn get_prices(
 		Err(eyre!("interval does not fit into collection interval"))?;
 	}
 
-	let amount: i32 = (amount * (interval / collection_interval_minutes)) as i32 * -1;
 	let pair =
 		Pair::get_pair(pair_string.as_str(), Some(CURRENT_CHAIN.into())).ok_or_eyre("invalid pair")?;
 	let mut connection = client.get_connection()?;
 
+	let offset: i32 = match before {
+		Some(timestamp) => {
+			let current: i64 = connection.lindex(format!("{}:timestamps", pair.to_string()), -1)?;
+			let collection_interval_secs = COLLECTION_INTERVAL.std_duration().as_secs() as i64;
+			(current / collection_interval_secs - timestamp / collection_interval_secs) as i32 + 1
+		}
+		None => 0,
+	};
+
+	let offset = (offset * -1) as isize;
+	let amount = (amount * (interval / collection_interval_minutes)) as isize * -1;
+
+	let current: i64 = connection.lindex(format!("{}:timestamps", pair.to_string()), -1)?;
+	let collection_interval_secs = COLLECTION_INTERVAL.std_duration().as_secs() as i64;
+
+	let amount = amount as isize;
+
 	let timestamps: Vec<i64> = connection.lrange(
 		format!("{}:timestamps", pair.to_string()),
-		amount as isize,
-		-1,
+		amount + offset,
+		-1 + offset,
 	)?;
 
-	let prices: Vec<f64> =
-		connection.lrange(format!("{}:prices", pair.to_string()), amount as isize, -1)?;
+	let prices: Vec<f64> = connection.lrange(
+		format!("{}:prices", pair.to_string()),
+		amount + offset,
+		-1 + offset,
+	)?;
 
 	let datapoints = zip(prices, timestamps)
 		.map(|(price, timestamp)| Datapoint::new(price, TimeType::Timestamp(timestamp)))
@@ -64,6 +84,7 @@ struct ErrorValue {
 struct DatapointRequestInfo {
 	interval: u16,
 	amount: Option<u16>,
+	before: Option<i64>,
 }
 
 #[get("/prices/{pair}")]
@@ -73,14 +94,22 @@ async fn prices_wrapper(
 	client: Data<Client>,
 ) -> impl Responder {
 	let pair = pair.into_inner();
-	let DatapointRequestInfo { interval, amount } = interval.into_inner();
+	let DatapointRequestInfo {
+		interval,
+		amount,
+		before,
+	} = interval.into_inner();
 	match get_prices(
 		pair,
 		client.as_ref(),
 		u16::min(MAX_DATAPOINTS, amount.unwrap_or(u16::MAX)),
 		interval,
+		before,
 	) {
-		Ok(prices) => HttpResponse::Ok().json(prices),
+		Ok(mut prices) => {
+			prices.reverse();
+			HttpResponse::Ok().json(prices)
+		}
 		Err(error) => HttpResponse::BadRequest().json(ErrorValue {
 			error: error.to_string(),
 		}),
